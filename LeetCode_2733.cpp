100f261343b023f5ecc6eccb04ec31d265d02427
Usama Razzaq <123981971+usamarazzaq90@users.noreply.github.com>
2024-06-24 14:48:36 +0500

    Update LeetCode_2733.cpp

--- LeetCode_2733.cpp
+++ LeetCode_2733.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    int findNonMinOrMax(vector<int>& nums) {
+        //Insertion Sort
+        if(nums.size()<=2) return -1;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i;j>0;j--){
+                if(nums[j]<nums[j-1]){
+                    swap(nums[j],nums[j-1]);
+                }
+            }
+        }
+        return nums[1];
+    }
+};
