4def4221ce27f0eafea2f9d23acaa8ea1fb374b5
Usama Razzaq <123981971+usamarazzaq90@users.noreply.github.com>
2024-06-30 11:17:22 +0500

    Create LeetCode_349.cpp

---
+++ LeetCode_349.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector <int> ans;
+        unordered_set<int> set1,set2;
+
+        for(auto it: nums1){
+            set1.insert(it);
+        } 
+        for(auto it: nums2){
+            set2.insert(it);
+        } 
+
+        for(auto it=set1.begin(); it!=set1.end();it++){
+            if(set2.find(*it)!=set2.end()){
+                ans.push_back(*it);
+            }
+        }
+
+        return ans;
+    }
+};
