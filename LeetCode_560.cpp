51fc6857d10492d8fbe2ac0dc1be84b004bda9c1
Usama Razzaq <123981971+usamarazzaq90@users.noreply.github.com>
2024-08-06 14:30:54 +0500

    Create LeetCode_560.cpp

---
+++ LeetCode_560.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+       unordered_map<int,int>mp;
+       mp[0]=1;
+       int count=0;
+       int preSum=0;
+       for(int i=0;i<nums.size();i++){
+        preSum+=nums[i];
+        count+=mp[preSum-k];
+        mp[preSum]++;
+       }
+       return count;
+    }
+};
