1b1a0dde09dd30637eccee40b6edf6a6372f87ab
Usama Razzaq <123981971+usamarazzaq90@users.noreply.github.com>
2024-09-24 21:04:36 +0500

    Update LeetCode_217.cpp

--- LeetCode_217.cpp
+++ LeetCode_217.cpp
@@ -3,9 +3,7 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1]){
-                return true;
-            }
+            if(nums[i]==nums[i+1]) return true;
         }
         return false;
     }
