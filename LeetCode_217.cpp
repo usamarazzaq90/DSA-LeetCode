f685f6b2b76d6b71eb650ee4d71d78105b89d197
Usama Razzaq <123981971+usamarazzaq90@users.noreply.github.com>
2024-09-25 09:00:33 +0500

    Update LeetCode_217.cpp

--- LeetCode_217.cpp
+++ LeetCode_217.cpp
@@ -1,3 +1,19 @@
+//Method 1: Using map
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        map <int,int> mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        for(auto x:mp){
+            if(x.second>1) return true;
+        }
+        return false;
+    }
+};
+
+//Method 2: Using nested loop
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
