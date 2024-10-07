2ae49ab3351104cdf6a5156b44edfa6297520a55
Usama Razzaq <123981971+usamarazzaq90@users.noreply.github.com>
2024-10-07 22:37:40 +0500

    Update LeetCode_3.cpp

--- LeetCode_3.cpp
+++ LeetCode_3.cpp
@@ -1,35 +1,22 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        vector< int > mpp(256,-1);
-        int l=0;
-        int r=0;
-        int maxlength=0;
-        while(r<s.size()) {
-            if(mpp[s[r]]!=-1) {
-                 if(mpp[s[r]]>=l) l = mpp[s[r]] +1;
-                 }
-           
-            mpp[s[r]] = r;
-            maxlength = max(maxlength,r-l+1);
+       int maxLength=0;
+       int l=0,r=0;
+       int n=s.size();
+       vector<int>hash(256,-1);
+       while(r<n){
+            if(hash[s[r]]!=-1){
+                if(hash[s[r]]>=l){
+                    l=hash[s[r]]+1;
+                }
+            }
+            hash[s[r]]=r;
+            int len=r-l+1;
+            maxLength=max(maxLength,len);
             r++;
-        }
-        return maxlength;
-    //    int maxLength=0;
-    //    int l=0,r=0;
-    //    int n=s.size();
-    //    vector<int>hash(256,-1);
-    //    while(r<n){
-    //         if(hash[s[r]]!=-1){
-    //             if(hash[s[r]]>=l){
-    //                 l=hash[s[r]]+1;
-    //             }
-    //             int len=r-l+1;
-    //             maxLength=max(maxLength,len);
-    //             hash[s[r]]=r;
-    //             r++;
-    //         }
-    //    }
-    //    return maxLength;
+            
+       }
+       return maxLength;
     }
 };
