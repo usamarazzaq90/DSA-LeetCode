f62a7b2e205c0ff066f84730908e5ff06ec40b18
Usama Razzaq <123981971+usamarazzaq90@users.noreply.github.com>
2024-10-25 23:02:51 +0500

    Update LeetCode_2677.js

--- JavaScript/LeetCode_2677.js
+++ JavaScript/LeetCode_2677.js
@@ -4,17 +4,25 @@
  * @return {Array}
  */
 var chunk = function(arr, size) {
-    let res=[];
-    let subArr=[];
-    for(let i=0;i<arr.length;i++){
-        subArr.push(arr[i]);
-        if(subArr.length===size){
-            res.push(subArr);
-            subArr=[]
-        }
-    }
-    if(subArr.length){
-        res.push(subArr)
+    //Method 1
+    // let res=[];
+    // let subArr=[];
+    // for(let i=0;i<arr.length;i++){
+    //     subArr.push(arr[i]);
+    //     if(subArr.length===size){
+    //         res.push(subArr);
+    //         subArr=[]
+    //     }
+    // }
+    // if(subArr.length){
+    //     res.push(subArr)
+    // }
+    // return res;
+
+    //Method 2
+    const res=[]
+    for(let i=0;i<arr.length;i+=size){
+        res.push(arr.slice(i, i+size));
     }
     return res;
 };
