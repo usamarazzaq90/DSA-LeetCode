ae1e2a88eefafa11f26ac4b2026bb5f4df58c2f1
Usama Razzaq <123981971+usamarazzaq90@users.noreply.github.com>
2024-10-25 16:44:20 +0500

    Update LeetCode_2727.js

--- JavaScript/LeetCode_2727.js
+++ JavaScript/LeetCode_2727.js
@@ -3,6 +3,16 @@
  * @return {boolean}
  */
 var isEmpty = function(obj) {
-    if(Object.keys(obj).length==0) return true
-    return false
+    //Method 1
+    // if(Object.keys(obj).length==0) return true
+    // return false
+
+    //Method 2
+    if(Array.isArray(obj)){
+        return obj.length===0
+    }
+    else{
+        let arr=Object.keys(obj)
+        return arr.length===0
+    }
 };
