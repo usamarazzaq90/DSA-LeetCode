ff4e3183bdec1e0ae5aa990f53e68a40397e742c
Usama Razzaq <123981971+usamarazzaq90@users.noreply.github.com>
2024-10-28 21:47:57 +0500

    Update LeetCode_2631.js

--- JavaScript/LeetCode_2631.js
+++ JavaScript/LeetCode_2631.js
@@ -5,24 +5,24 @@
 Array.prototype.groupBy = function(fn) {
 
     //Method # 1
-    const res={};
-    for(let i=0;i<this.length;i++){
-        let key=fn(this[i]);
-        if(!res.hasOwnProperty(key)){
-            res[key]=[]
-        }
-        res[key].push(this[i])
-    }
-    return res;
-
-    // Method #  2 
     // const res={};
-    // for(const obj of this){
-    //     let key=fn(obj);
-    //     res[key]=res[key] || []; //Logical OR
-    //     res[key].push(obj)
+    // for(let i=0;i<this.length;i++){
+    //     let key=fn(this[i]);
+    //     if(!res.hasOwnProperty(key)){
+    //         res[key]=[]
+    //     }
+    //     res[key].push(this[i])
     // }
     // return res;
+
+    // Method #  2 
+    const res={};
+    for(const obj of this){
+        let key=fn(obj);
+        res[key]=res[key] || []; //Logical OR
+        res[key].push(obj)
+    }
+    return res;
 };
 
 /**
