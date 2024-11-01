fbb4f97bc84b0f0eb2688bb7f2399e3dd5bb4b88
Usama Razzaq <123981971+usamarazzaq90@users.noreply.github.com>
2024-11-01 06:42:03 +0500

    Update LeetCode_26.cpp

--- LeetCode_26.cpp
+++ LeetCode_26.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+        //Method # 1
         int insertIndex=1;
         for(int i=1;i<nums.size();i++){
             if(nums[i-1]!=nums[i]){
@@ -9,5 +10,18 @@ public:
             }
         }
         return insertIndex;
+        
+        //Method # 2 (Using Hashmap)
+        // map <int, int> mpp;
+        // for(int i=0;i<nums.size();i++){
+        //     if(mpp.find(nums[i])==mpp.end()){
+        //         mpp[nums[i]]=i;
+        //     }
+        // }
+        // nums.clear();
+        // for(auto it : mpp){
+        //     nums.push_back(it.first);
+        // }
+        // return nums.size();
     }
 };
