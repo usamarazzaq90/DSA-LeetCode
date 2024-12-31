4841c78f582b068114963c78cc67955741932a6a
Usama Razzaq <123981971+usamarazzaq90@users.noreply.github.com>
2024-12-31 20:56:37 +0500

    Update LeetCode_125.cpp

--- LeetCode_125.cpp
+++ LeetCode_125.cpp
@@ -3,16 +3,10 @@ public:
     bool isPalindrome(string s) {
         int left=0;
         int right=s.size()-1;
-        while(left<right){
-            if(!isalnum(s[left])){
-                left++;
-            }
-            else if(!isalnum(s[right])){
-                right--;
-            }
-            else if(tolower(s[left])!=tolower(s[right])){
-                return false;
-            }
+        while(left<=right){
+            if(!isalnum(s[left])) left++;
+            else if(!isalnum(s[right])) right--;
+            else if(tolower(s[left])!=tolower(s[right])) return false;
             else{
                 left++;
                 right--;
