733f71f685de04f4ae0fb42c9a1c04581fbcddf6
Usama Razzaq <123981971+usamarazzaq90@users.noreply.github.com>
2025-01-01 11:43:05 +0500

    Update LeetCode_771.cpp

--- LeetCode_771.cpp
+++ LeetCode_771.cpp
@@ -10,11 +10,21 @@ public:
         // }
 
         // Method # 2
-        for(auto i: stones ){
-            for(auto  j: jewels){
-                if (i==j) count++;
-            }
+        // for(auto i: stones ){
+        //     for(auto  j: jewels){
+        //         if (i==j) count++;
+        //     }
+        // }
+
+        // Method # 3   (Using map)
+        map <char,int> mp;
+        for(int i=0;i<jewels.size();i++){
+            mp[jewels[i]]++;
         }
+        for(int j=0;j<stones.size();j++){
+            if (mp[stones[j]]) count++;
+        }
+        
         return count;
     }
 };
